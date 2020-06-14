539be0f781976e87d6c111a102ac00c86971fa65
Andrey Sergeev <andreyser07@gmail.com>
2020-06-14 21:36:17 +0700

    Rename file and change error handling
    Change error handling due to the addition of general error-handling
    middleware.

--- controllers/errorLinkValidation.js
+++
@@ -1,8 +0,0 @@
-module.exports.checkLinkValidation = (res, err) => {
-  if (err.name === 'ValidationError') {
-    res.status(400);
-  } else {
-    res.status(500);
-  }
-  res.send({ data: err.message });
-};
